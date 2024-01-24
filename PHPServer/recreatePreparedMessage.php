a21fab55ad0e79445caa3cae5677e0f36f4934b8
Jörg Eisfeld <github@jeisfeld.de>
2024-01-24 12:33:25 +0100

    Do not update lasttimestamp with preparedMessage

--- PHPServer/recreatePreparedMessage.php
+++ PHPServer/recreatePreparedMessage.php
@@ -24,11 +24,8 @@ if ($isSlave && ($aiRelation['aiPolicy'] == 1 || $aiRelation['aiPolicy'] == 4)) 
     $preparedMessage = $result['success'] ? $result['message']['content'] : "";
     echo $preparedMessage;
     
-    $currentDateTime = new DateTime();
-    $mysqlTimestamp = substr($currentDateTime->format("Y-m-d H:i:s.u"), 0, 23);
-    
-    $stmt = $conn->prepare("UPDATE dsm_conversation SET prepared_message = ?, lasttimestamp = ? WHERE id = ? and relation_id = ?");
-    $stmt->bind_param("sssi", $preparedMessage, $mysqlTimestamp, $conversationId, $relationId);
+    $stmt = $conn->prepare("UPDATE dsm_conversation SET prepared_message = ? WHERE id = ? and relation_id = ?");
+    $stmt->bind_param("ssi", $preparedMessage, $conversationId, $relationId);
     $stmt->execute();
     $stmt->close();
     sendAdminMessage($conn, $username, $password, $relationId, "CONVERSATION_EDITED", [
--- PHPServer/setPreparedMessage.php
+++ PHPServer/setPreparedMessage.php
@@ -18,11 +18,8 @@ $relationId = @$_POST['relationId'];
 $isSlave = @$_POST['isSlave'];
 
 if ($isSlave) {
-    $currentDateTime = new DateTime();
-    $mysqlTimestamp = substr($currentDateTime->format("Y-m-d H:i:s.u"), 0, 23);
-    
-    $stmt = $conn->prepare("UPDATE dsm_conversation SET prepared_message = ?, lasttimestamp = ? WHERE id = ? and relation_id = ?");
-    $stmt->bind_param("sssi", $preparedMessage, $mysqlTimestamp, $conversationId, $relationId);
+    $stmt = $conn->prepare("UPDATE dsm_conversation SET prepared_message = ? WHERE id = ? and relation_id = ?");
+    $stmt->bind_param("ssi", $preparedMessage, $conversationId, $relationId);
     $stmt->execute();
     $stmt->close();
     sendAdminMessage($conn, $username, $password, $relationId, "CONVERSATION_EDITED", [
